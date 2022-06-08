455df6ec731fcb8f920ad70a05314de4faa8c013
rezcode <rezharians@gmail.com>
2022-06-08 18:23:56 +0800

    insert Week 2 - Tugas Javascript Intro 1

---
+++ Tugas 1/soal1.js
@@ -0,0 +1,23 @@
+const biodata = {
+  name: "Rezha Riansyah R",
+  age: "",
+  hobbies: ["futsal", "music", "gaming"],
+  isMarried: false,
+  schoolList: [
+    {
+      name: "Rezha Riansyah R",
+      yearIn: 2012,
+      yearOut: 2017,
+      major: "Teknik Informatika",
+    },
+  ],
+  skills: [
+    {
+      name: "javascript",
+      level: "beginner",
+    },
+  ],
+  interestInCoding: true,
+};
+
+console.log(biodata);
---
+++ Tugas 1/soal2.js
@@ -0,0 +1,39 @@
+const mtk = 80;
+const bahasaIndonesia = 90;
+const bahasaInggris = "balba1";
+const ipa = 69;
+const arr = [];
+
+arr.push(mtk, bahasaIndonesia, bahasaInggris, ipa);
+
+let average = arr.reduce((a, b) => a + b) / arr.length;
+
+const output = () => {
+  if (average >= 90 && average <= 100) {
+    return `Rata-rata = ${average}
+Grade = A`;
+  } else if (average >= 80 && average <= 89) {
+    return `Rata-rata = ${average}
+Grade = B`;
+  } else if (average >= 70 && average <= 79) {
+    return `Rata-rata = ${average}
+Grade = C`;
+  } else if (average >= 60 && average <= 69) {
+    return `Rata-rata = ${average}
+Grade = D`;
+  } else {
+    return `Rata-rata = ${average}
+Grade = E`;
+  }
+};
+
+if (
+  typeof mtk !== "number" ||
+  typeof bahasaIndonesia !== "number" ||
+  typeof bahasaInggris !== "number" ||
+  typeof ipa !== "number"
+) {
+  console.log("must be number");
+} else {
+  console.log(output());
+}
