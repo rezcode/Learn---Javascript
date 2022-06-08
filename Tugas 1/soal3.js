d850fd1a68c53dca9829453d27447e6eb64247c1
rezcode <rezharians@gmail.com>
2022-06-08 20:02:40 +0800

    update soal3

--- Tugas 1/soal3.js
+++ Tugas 1/soal3.js
@@ -4,12 +4,11 @@ let output = "";
 if (typeof jumlah !== "number") {
   console.log(`Data harus number`);
 } else {
-  for (let i = 1; i <= jumlah; i++) {
-    for (let j = i; j <= jumlah; j++) {
+  for (var i = jumlah; i >= 1; i--) {
+    for (var j = 1; j <= i; j++) {
       output += j;
     }
-    output += `\n`;
+    output += "\n";
   }
-
   console.log(output);
 }
